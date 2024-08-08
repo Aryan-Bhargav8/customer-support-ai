576c780c821208046e9a5bd69adcf609bf9e0a7a
shatha <shatha200>
2024-08-08 19:36:28 +0100

    removed unused css  + change fonts and color

--- app/page.js
+++ app/page.js
@@ -22,34 +22,35 @@ export default function LandingPage() {
       flexDirection="column"
       sx={{
         overflow: 'hidden',
-        fontFamily: 'roboto',
+        fontFamily: 'poppins',
          }}
     >
    
 
       <Box
-        width="100%"
-        height="calc(100% - 64px)"
+        width="100vw"
+        height="100vh"
         display="flex"
         flexDirection="column"
-        alignItems="center"
         justifyContent="center"
+        alignItems="center"
         sx={{
             backgroundImage: 'url(1.gif)',
             backgroundPosition: 'center', 
             backgroundRepeat: 'no-repeat',
             backgroundSize:'cover',
+            color:'white',
           }}
        
       >
         <Box display="flex" flexDirection="column" alignItems="center" mb={4} p={4}  spacing={2}   borderRadius={10} >
-          <Typography variant="h2"  mb={2} sx={{ fontWeight: 'bold', textAlign: 'center',fontFamily:'roboto' }}>
+          <Typography variant="h2"  mb={2} sx={{ fontWeight: 'bold', textAlign: 'center',fontFamily:'Poppins', color:'#D9D9D9' }}>
           Welcome to Headstarter AI Support: 
           </Typography>
-          <Typography variant="h3"  mb={2} sx={{ fontWeight: 'bold', textAlign: 'center',fontFamily:'roboto' }}>
+          <Typography variant="h3"  mb={2} sx={{ fontWeight: 'bold', textAlign: 'center',fontFamily:'Poppins' ,color:'#D9D9D9' }}>
           Your Partner in AI-Powered Interviews for Software Engineering Jobs!
           </Typography>
-          <Typography variant="h6" mb={4} sx={{ textAlign: 'center' ,fontFamily:'roboto' }}>
+          <Typography variant="h6" mb={4} sx={{ fontWeight: 'bold',textAlign: 'center' ,fontFamily:'Poppins',  color:'#D9D9D9'}}>
           We are thrilled to assist you in navigating our platform, where AI technology meets the world of software engineering interviews. Whether you're a candidate preparing for your next role or a recruiter seeking top talent, Headstarter AI Support is here to ensure a seamless experience.
           </Typography>
           <Button
@@ -57,12 +58,12 @@ export default function LandingPage() {
             color="success"
             size="large"
             onClick={handleGetStarted}
-            sx={{fontFamily:'roboto',
+            sx={{fontFamily:'Quicksand',
               mt: 2,
-              backgroundColor: '#669999',
+              backgroundColor: '#6F9DFF',
               color:'black',
               '&:hover': {
-                backgroundColor: '#527a7a',
+                backgroundColor: '#9980ff',
               },
               padding: '10px 20px',
               borderRadius: '20px',
