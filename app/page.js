1db21a96688a2e5e7db51f9f365c1518acc12766
Aryan-Bhargav8 <bhargav.aryan8@gmail.com>
2024-08-09 23:42:56 +0530

    added microinteraction for button and fixed the appbar

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { Box, Typography, Button ,AppBar,Toolbar,IconButton,Avatar} from '@mui/material';
+import { Box, Typography, Button, AppBar, Toolbar, IconButton, Avatar } from '@mui/material';
 import { useRouter } from 'next/navigation';
 
 export default function LandingPage() {
@@ -12,7 +12,7 @@ export default function LandingPage() {
     router.push('/Chat');
   };
 
-  
+
 
   return (
     <Box
@@ -23,23 +23,8 @@ export default function LandingPage() {
       sx={{
         overflow: 'hidden',
         fontFamily: 'poppins',
-         }}
+      }}
     >
-      <AppBar position="static" sx={{ backgroundColor: '#020929', padding: '10px 20px' }}>
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <Avatar  src="/logo.jpg" alt="logo" sx={{ width: 56, height: 56 }}/>
-      <IconButton
-        edge="start"
-        color="inherit"
-        aria-label="logo"
-        sx={{ mr: 2 ,fontFamily: 'poppins',padding:"20px"}}
-      > SupportIQ
-      </IconButton>
-      </Box>
-      </Toolbar>
-      </AppBar>
-
       <Box
         width="100vw"
         height="100vh"
@@ -48,41 +33,69 @@ export default function LandingPage() {
         justifyContent="center"
         alignItems="center"
         sx={{
-            backgroundImage: 'url(1.gif)',
-            backgroundPosition: 'center', 
-            backgroundRepeat: 'no-repeat',
-            backgroundSize:'cover',
-            color:'white',
-          }}
-       
+          backgroundImage: 'url(1.gif)',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          color: 'white',
+        }}
+
       >
-        
-        <Box display="flex" flexDirection="column" alignItems="center" mb={4} p={4}  spacing={2}   borderRadius={10} >
-          <Typography variant="h2"  mb={2} sx={{ fontWeight: 'bold', textAlign: 'center',fontFamily:'Poppins', color:'#D9D9D9' }}>
-          Welcome to SupportIQ: 
+        <AppBar position="absolute" elevation={0} sx={{ backgroundColor: 'transparent', padding: '10px 20px' }}>
+          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Box sx={{ position:'absolute', display: 'flex', alignItems: 'center' }}>
+              <Avatar src="/logo.jpg" alt="logo" sx={{ width: 56, height: 56 }} />
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="logo"
+                sx={{ mr: 2, fontFamily: 'poppins', padding: "20px" }}
+              > SupportIQ
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </AppBar>
+
+        <Box display="flex" flexDirection="column" alignItems="center" mb={4} p={4} spacing={2} borderRadius={10} sx={{ backdropFilter: 'blur(8px)' }}>
+          <Typography variant="h2" mb={2} sx={{ fontWeight: 'bold', textAlign: 'center', fontFamily: 'Poppins', color: '#D9D9D9' }}>
+            Welcome to SupportIQ:
           </Typography>
-          <Typography variant="h3"  mb={2} sx={{ fontWeight: 'bold', textAlign: 'center',fontFamily:'Poppins' ,color:'#D9D9D9' }}>
-          your all-knowing, always-on digital assistant!
+          <Typography variant="h3" mb={2} sx={{ fontWeight: 'bold', textAlign: 'center', fontFamily: 'Poppins', color: '#D9D9D9' }}>
+            your all-knowing, always-on digital assistant!
           </Typography>
 
-          <Typography variant="h6" mb={4} sx={{ fontWeight: 'bold',textAlign: 'center' ,fontFamily:'Poppins',  color:'#D9D9D9'}}>
+          <Typography variant="h6" mb={4} sx={{ fontWeight: 'bold', textAlign: 'center', fontFamily: 'Poppins', color: '#D9D9D9' }}>
             Think of me as your personal helper,designed to make your customer service experience seamless and efficient. Whether it&apos;s day or night .I am here to ensure you get the support you need .
           </Typography>
+        </Box>
+
+        {/* button */}
+        <Box
+          sx={{
+            borderRadius: '20px',
+            backgroundColor: '#6F9DFF',
+            '&:hover': {
+              backgroundColor: '#9980ff',
+            }
+          }}>
           <Button
             variant="contained"
             color="success"
             size="large"
             onClick={handleGetStarted}
-            sx={{fontFamily:'Quicksand',
-              mt: 2,
+            sx={{
+              fontFamily: 'Quicksand',
               backgroundColor: '#6F9DFF',
-              color:'black',
+              color: 'black',
+              position: 'relative',
+              bottom: '5px',
+
               '&:hover': {
                 backgroundColor: '#9980ff',
+                bottom: '0px',
               },
               padding: '10px 20px',
               borderRadius: '20px',
-              textTransform: 'none',
               fontSize: '20px',
               fontWeight: 'bold',
               display: 'block',
@@ -92,7 +105,7 @@ export default function LandingPage() {
           </Button>
         </Box>
 
-        
+
 
       </Box>
     </Box>
