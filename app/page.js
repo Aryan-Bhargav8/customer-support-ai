42c118505d10d6682a1db2c85746debfe37bbb45
Aryan Bhargav <108352122+Aryan-Bhargav8@users.noreply.github.com>
2024-08-09 22:04:06 +0530

    fixed apostrophe errors that caused deployment issues page.js

--- app/page.js
+++ app/page.js
@@ -66,7 +66,7 @@ export default function LandingPage() {
           </Typography>
 
           <Typography variant="h6" mb={4} sx={{ fontWeight: 'bold',textAlign: 'center' ,fontFamily:'Poppins',  color:'#D9D9D9'}}>
-            Think of me as your personal helper,designed to make your customer service experience seamless and efficient. Whether it's day or night .I'am here to ensure you get the support you need .
+            Think of me as your personal helper,designed to make your customer service experience seamless and efficient. Whether it&apos;s day or night .I am here to ensure you get the support you need .
           </Typography>
           <Button
             variant="contained"
